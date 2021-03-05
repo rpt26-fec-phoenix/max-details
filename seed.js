66787e9b207a18050ed47ea276334d1b2ad64b7e
Max Proano <proanom89@gmail.com>
2021-03-04 20:20:38 -0800

    Add record to db

--- seed.js
+++ seed.js
@@ -8,6 +8,49 @@ db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function() {
   console.log('We are connected!');
 });
+
+// define details schema
+const detailSchema = new mongoose.Schema({
+  typeOfPlace: String,
+  propertyType: String,
+  cancellation: String,
+  houseRules: {
+    checkin: String,
+    checkout: String,
+    selfCheckIn: {
+      allowSelfCheckIn: Boolean,
+      howToCheckIn: String
+    },
+    suitableAge: String,
+    smoking: Boolean,
+    parties: Boolean,
+    pets: Boolean,
+    longTermStay: Boolean,
+    additionalRules: String
+  },
+  about: {
+    description: String,
+    access: String,
+    other: String
+  },
+  sleepingArrangements: {
+    guests: Number,
+    bedrooms: Number,
+    beds: Number,
+    baths: Number
+  },
+  propertyId: Number
+});
+
+// compile schema into model
+const Detail = mongoose.model('Detail', detailSchema);
+
 // create record
+const newDetail = new Detail({
+  typeOfPlace: 'House'
+});
 
 // add record to db
+newDetail.save(function (err) {
+  if (err) { return console.error(err); }
+});
