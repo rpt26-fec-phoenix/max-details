8a3b45ab32c6f5067b891538eba34cc07e4c87ea
Max Proano <proanom89@gmail.com>
2021-03-12 19:15:15 -0800

    Start setting up additional enzyme tests

--- test/client.test.js
+++ test/client.test.js
@@ -6,6 +6,7 @@ import Title from '../client/components/Title.jsx';
 import Details from '../client/components/Details.jsx';
 import About from '../client/components/About.jsx';
 import Detail from '../client/components/Detail.jsx';
+import { sampleDetail, sampleSelfChecKInAllowedDetail, sampleEntirePlaceDetail, sampleHost, sampleSuperHost} from './sample-data.js';
 
 describe('Shallow rendering', () => {
   test('App should contain Title, Detail, and About components', () => {
@@ -16,5 +17,24 @@ describe('Shallow rendering', () => {
   test('Details should contain Detail component', () => {
     const wrapper = shallow(<Details />);
     expect(wrapper.contains(<Detail />)).toBe(true);
+  });
+});
+
+describe('Detail rendering logic', () => {
+  test('Details should include superHost if host is superHost', () => {
+    const wrapper = shallow(<Details details={sampleDetail} host={sampleSuperHost}/>);
+    expect(wrapper.contains(<Detail type={'superHost'} details={sampleDetail} host={sampleSuperHost}/>));
+  });
+
+  xtest('Details should include selfCheckIn if host is not superHost and selfCheckIn is allowed', () => {
+
+  });
+
+  xtest('Details should include entirePlace if property is entirePlace', () => {
+
+  });
+
+  xtest('Details should only include enhancedClean, cancellation, and houseRules if not superHost, selfCheckIn, or entirePlace', () => {
+
   });
 });
