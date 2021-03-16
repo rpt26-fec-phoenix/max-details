d2c872712a7c47f1bd79feebb5b5beba68093d4e
Max Proano <proanom89@gmail.com>
2021-03-16 16:38:43 -0700

    Init styled components and refactored index.html

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledTitle = styled.div`
+  font-family: Arial, Helvetica, sans-serif;
+`;
 
 const Title = ({details, host}) => {
   return (
-    <div>
+    <StyledTitle>
       <div>{details.typeOfPlace} in {details.propertyType} hosted by {host.Hostname}</div>
       <div>{details.sleepingArrangements.guests} guests {details.sleepingArrangements.bedrooms} rooms  {details.sleepingArrangements.beds} beds {details.sleepingArrangements.baths} baths</div>
-    </div>
+    </StyledTitle>
   );
 };
 
